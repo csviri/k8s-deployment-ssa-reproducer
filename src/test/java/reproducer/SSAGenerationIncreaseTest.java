1eb6d37349c30c4bb9fa03f3fa4ab458f9cfa32d
Attila Mészáros <csviri@gmail.com>
2024-02-24 15:37:32 +0100

    found issue
    Signed-off-by: Attila Mészáros <csviri@gmail.com>

--- src/test/java/reproducer/SSAGenerationIncreaseTest.java
+++ src/test/java/reproducer/SSAGenerationIncreaseTest.java
@@ -1,15 +1,21 @@
 package reproducer;
 
+import io.fabric8.kubernetes.api.model.ConfigMapBuilder;
+import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
 import io.fabric8.kubernetes.client.KubernetesClientBuilder;
 import io.fabric8.kubernetes.client.utils.KubernetesSerialization;
 import org.junit.jupiter.api.Test;
+
+import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SSAGenerationIncreaseTest {
 
     public static final String ANNOTATION_KEY = "io.test";
+//    ok found:
+//    https://midbai.com/en/post/meta-generation-increasing-strategy-exploration/#generationchangedpredicate-filter-status-principle
 
     @Test
     void generationIncreasesWithJustAddingAnnotation() {
@@ -27,6 +33,26 @@ public class SSAGenerationIncreaseTest {
 
         // this fails
         assertThat(updatedAnnotation.getMetadata().getGeneration()).isEqualTo(updatedWithoutAnnotation.getMetadata().getGeneration());
+    }
+
+    @Test
+    void ifNoSSAGenerationDoesNotIncrease() {
+        var c = new KubernetesClientBuilder().build();
+        var def = new KubernetesSerialization().unmarshal(deployment, Deployment.class);
+        def.getMetadata().setName("test"+System.currentTimeMillis());
+
+        var created = c.resource(def).create();
+
+        created.getMetadata().getAnnotations().put(ANNOTATION_KEY,""+System.currentTimeMillis());
+        created.getMetadata().setResourceVersion(null);
+        var updatedAnnotation = c.resource(created).update();
+
+        // this fails
+        assertThat(created.getMetadata().getGeneration()).isEqualTo(updatedAnnotation.getMetadata().getGeneration());
+    }
+
+    void testConfigMap() {
+
     }
 
     String deployment = """
